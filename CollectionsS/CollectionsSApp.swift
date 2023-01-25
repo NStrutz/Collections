0eafbda3f5c1cfe3925d51b0c5a3d70968985e26
NStrutz <nic@4strutz.de>
2023-01-25 18:05:50 +0100

    Initial Commit

---
+++ CollectionsS/CollectionsSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionsSApp.swift
+//  CollectionsS
+//
+//  Created by Nic-Alexander Strutz on 25.01.23.
+//
+
+import SwiftUI
+
+@main
+struct CollectionsSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
