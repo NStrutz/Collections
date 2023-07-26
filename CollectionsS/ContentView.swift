fd379c67f51a907cac9e8d16428059919aee693e
NStrutz <nic@4strutz.de>
2023-07-26 20:07:25 +0200

    Added feature so that number of items updates (after many tries) (It was very simple) Added images to items (very crude/rough)

--- CollectionsS/ContentView.swift
+++ CollectionsS/ContentView.swift
@@ -1,18 +1,32 @@
+//
+//
+//Developed by Nic-Alexander Strutz for a Computer Science IA
+//Credits to ChatGPT
+// Version: 26 July 2023
+
+
 import SwiftUI
 import Foundation
+import PhotosUI
 
 //Global Variables
 var isOnItemView = false
 class Item: Identifiable, Codable {
     var id = UUID()
     var title:String
+    var photo: Data?
     
     init(title:String){
         self.title = title
     }
+    init(title:String, photo:Data){
+        self.title = title
+        self.photo = photo
+    }
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(title, forKey: .title)
+        try container.encode(photo, forKey: .photo)
     }
 }
 class Collection: Codable, Identifiable {
@@ -55,7 +69,7 @@ extension Item: Hashable {
 
 struct ContentView: View {
   @State private var collections = [ //Sample Text used for testing
-    Collection(title:"Collection 1", description: "Sample 1", items:[Item(title:"Item A"),Item(title:"Item B")])]
+    Collection(title:"", description: "", items:[Item(title:"")])]
   @State private var newCollectionTitle = ""
   @State private var newItemTitle = ""
   @State private var newDescription = ""
@@ -63,6 +77,7 @@ struct ContentView: View {
   @State private var isAddingCollection = false
   @State private var isAddingItem = false
   @State private var AddCollectionError = false
+  @State private var updater = false
 
 
     var body: some View {
@@ -70,10 +85,12 @@ struct ContentView: View {
             NavigationView {
                 List {
                     ForEach($collections, id: \.self) { $collection in
-                        NavigationLink(destination: DetailView(collectionTitle: collection.title,collectionDescription: collection.description,list:$collection.items)) {
+                        NavigationLink(destination: DetailView(collectionTitle: collection.title,collectionDescription: collection.description,list:$collection.items, updater: $updater)) {
                             HStack{
                                 Text(collection.title)
                                 Spacer()
+                                Text(String(updater))
+                                    .hidden()
                                 Text(String(collection.items.count) + " Items")
                                     .foregroundColor(.gray)
                             }
@@ -86,7 +103,7 @@ struct ContentView: View {
                     NewCollectionView(isAddingCollection: $isAddingCollection,AddCollectionError: $AddCollectionError, collections: $collections, newCollectionTitle: $newCollectionTitle, newDescription: $newDescription, newItems: $newItems)
                 }
                 .sheet(isPresented:$isAddingItem){
-                    NewItemView(isAddingItem:$isAddingItem, collections: $collections, newItemTitle: $newItemTitle)
+                    NewItemView(isAddingItem:$isAddingItem, collections: $collections, newItemTitle: $newItemTitle, updater: $updater)
                 }
             }
             
@@ -152,14 +169,27 @@ struct DetailView: View {
   var collectionTitle: String
   var collectionDescription: String
   @Binding var list: [Item]
+  @Binding var updater: Bool
   @State private var showingPopover = false
+  @State private var photoData = Data()
 
     var body: some View {
         ZStack{
                 List {
                     ForEach(list, id: \.self) { item in
                         NavigationLink(destination:ItemView(ItemTitle: item.title)) {
-                            Text(item.title)
+                            HStack{
+                                if let photoData = item.photo {
+                                    if let image = UIImage(data: photoData){
+                                        Image(uiImage:image)
+                                         .resizable()
+                                         .aspectRatio(contentMode: .fill)
+                                         .frame(width: 50, height: 50)
+                                         .clipped()
+                                    }
+                                }
+                                Text(item.title)
+                            }
                         }
                     }
                     .onDelete(perform: delete)
@@ -185,6 +215,7 @@ struct DetailView: View {
     }
     func delete(at offsets: IndexSet) {
       list.remove(atOffsets: offsets)
+      updater = !updater
     }
   }
 
@@ -263,30 +294,58 @@ struct NewItemView: View {
     @Binding var isAddingItem: Bool
     @Binding var collections: [Collection]
     @Binding var newItemTitle: String
+    @Binding var updater: Bool
     @State private var selectedCollection: Collection? = nil
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var selectedPhotoData: Data?
 
     var body: some View {
       NavigationView {
           if selectedCollection == nil {
-              List{
-                  ForEach(collections, id: \.self) { name in
-                      Button(action: {
-                          selectedCollection = name
-                      }){
-                          Text(name.title)
+              VStack{
+                  List{
+                      ForEach(collections, id: \.self) { name in
+                          Button(action: {
+                              selectedCollection = name
+                          }){
+                              Text(name.title)
+                          }
                       }
-                }
+                  }
               }
+              .navigationBarTitle(Text("Select Collection"))
           }
           else {
               VStack {
                   Divider()
                   TextField("Item title", text: $newItemTitle)
                       .padding(.horizontal)
+                  HStack{
+                      if let selectedPhotoData,
+                         let image = UIImage(data: selectedPhotoData){
+                          Image(uiImage:image)
+                              .resizable()
+                              .aspectRatio(contentMode: .fill)
+                              .frame(width: 50, height: 50, alignment: .leading)
+                      }
+                      Spacer()
+                          .frame(width: 100)
+                      PhotosPicker(selection: $selectedItem, matching: .any(of: [.images, .not(.livePhotos)])){
+                          Text("Select a Photo")
+                      }
+                      .onChange(of: selectedItem) { newImage in
+                          Task {
+                              if let data = try? await newImage?.loadTransferable(type: Data.self) {
+                                  selectedPhotoData = data
+                              }
+                          }
+                      }
+                  }
                   Spacer()
                   Button(action: {
                       if let selectedCollection = selectedCollection {
-                          selectedCollection.items.append(Item(title:newItemTitle))
+                          selectedCollection.items.append(Item(title:newItemTitle, photo:selectedPhotoData ?? Data())) // could add what to do if no image is added
+                          updater = !updater
                       }
                       self.isAddingItem = false
                       self.newItemTitle = ""
@@ -331,3 +390,5 @@ struct ContentView_Previews: PreviewProvider {
 
 //Add a message to add item screen when there is no collection
 //Delete data when closing add screen
+//Add rewards system??
+//find a way to do all the error handeling
